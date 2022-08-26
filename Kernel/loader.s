526839dc7f02026fcc4f59c72fce44ed3f38573d
dead-tech <dev.dead.tech@gmail.com>
2022-08-26 20:54:08 +0200

    :bug: loader.s: Removed unnecessary colon that was causing the assembler to produce wrong output

--- Kernel/loader.s
+++ Kernel/loader.s
@@ -1,6 +1,6 @@
 global loader                   ; the entry symbol for ELF
 
-extern main 
+extern main
 
 KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes
 
@@ -14,7 +14,7 @@ align 4                         ; align at 4 bytes
 kernel_stack:                   ; label points to beginning of memory
     resb KERNEL_STACK_SIZE      ; reserve stack for the kernel
 
-section .text:                  ; start of the text (code) section
+section .text                  ; start of the text (code) section
 align 4                         ; the code must be 4 byte aligned
     dd MAGIC_NUMBER             ; write the magic number to the machine code,
     dd FLAGS                    ; the flags,
@@ -26,7 +26,7 @@ loader:                         ; the loader label (defined as entry point in li
     mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
 
     add  esp, 4
-    push ebx                    ; multiboot info in ebx 
+    push ebx                    ; multiboot info in ebx
     call main
 
 .loop:
