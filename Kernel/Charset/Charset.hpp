d453cd5d2c827bfcebc027c35bfb078cef5c8b83
dead-tech <dev.dead.tech@gmail.com>
2022-08-26 20:55:16 +0200

    :sparkles: Charset.hpp: Define some ASCII characters constants in hex format

---
+++ Kernel/Charset/Charset.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace Charset {
+constexpr static auto BACKSPACE       = 0x08;
+constexpr static auto TAB             = 0x09;
+constexpr static auto NEWLINE         = 0x0A;
+constexpr static auto CARRIAGE_RETURN = 0x0D;
+constexpr static auto SPACE           = 0x20;
+} // namespace Charset
