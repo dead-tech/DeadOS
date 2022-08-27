94de8f1dde6d795afb9fe1bdfd60dda6db808d60
dead-tech <dev.dead.tech@gmail.com>
2022-08-27 10:31:28 +0200

    :sparkles: Entry point to test framebuffer and serial port

--- Kernel/kmain.cpp
+++ Kernel/kmain.cpp
@@ -1,3 +1,4 @@
+#include "Io/SerialPort.hpp"
 #include "Screen/Framebuffer.hpp"
 
 int main()
@@ -6,5 +7,8 @@ int main()
     framebuffer.clear();
     char buffer[] = "Welcome to DeadOS";
     framebuffer.write_cstr(buffer);
+
+    Io::SerialPort com1_port = Io::SerialPort::init(Io::COM1_SERIAL_PORT);
+    com1_port.write_cstr("COM1: Hello sailor!");
     return 0;
 }
