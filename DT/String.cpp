54605e2c8e1b40dece7f14199f225d3f7c7240e2
dead-tech <dev.dead.tech@gmail.com>
2022-08-27 10:27:53 +0200

    :sparkles: DT: Simple implementation of strlen and strcpy

--- DT/String.cpp
+++ DT/String.cpp
@@ -1,1 +1,22 @@
 #include "String.hpp"
+
+namespace dt {
+
+u32 strlen(const char *str)
+{
+    u32 len = 0;
+    while (str[len] != 0) { len++; }
+
+    return len;
+}
+
+void strcpy(char *dst, const char *src)
+{
+    u32 len = 0;
+
+    for (u32 i = 0; src[len] != 0; ++i) { dst[len] = src[len]; }
+
+    dst[len] = '\0';
+}
+
+} // namespace dt
--- DT/String.hpp
+++ DT/String.hpp
@@ -1,5 +1,12 @@
 #pragma once
 
-class String
-{
-};
+#include <DT/Types.hpp>
+
+namespace dt {
+
+u32  strlen(char *str);
+void strcpy(char *dst, char *src);
+
+char *itoa(u64 integer, u8 base = 10);
+
+} // namespace dt
