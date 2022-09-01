68cea7de0f2cf89e7294cfdad37bd5dc239df63d
dead-tech <dev.dead.tech@gmail.com>
2022-09-01 11:25:45 +0200

    :recycle: Kernel: Improve handler for Interrupt Service Routines

--- Kernel/DescriptorTables/InterruptServiceRoutine.cpp
+++ Kernel/DescriptorTables/InterruptServiceRoutine.cpp
@@ -4,7 +4,14 @@ namespace Isr {
 
 static HandlerFnPtr interrupt_handlers[256];
 
-void isr_handler([[maybe_unused]] CpuRegisters registers) { Screen::Framebuffer::write_cstr("Received interrupt: \n"); }
+void isr_handler([[maybe_unused]] CpuRegisters registers)
+{
+    if (interrupt_handlers[registers.interrupt_number] != 0) {
+        interrupt_handlers[registers.interrupt_number](registers);
+    } else {
+        debug("[WARNING]: Unhandled interrupt!");
+    }
+}
 
 void register_interrupt_handler(dts::u8 index, HandlerFnPtr handler) { interrupt_handlers[index] = handler; }
 
