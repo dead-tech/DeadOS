443abe43cc0e4bb295382f1e8d908ba5303c3e60
dead-tech <dev.dead.tech@gmail.com>
2022-09-06 10:19:17 +0200

    :rocket: dts: Starting point for String class

--- dts/String.cpp
+++ dts/String.cpp
@@ -14,9 +14,385 @@ void strcpy(char *dst, const char *src)
 {
     u32 len = 0;
 
-    for (u32 i = 0; src[len] != 0; ++i) { dst[len] = src[len]; }
+    for (u32 i = 0; src[len] != 0; ++i) { dst[i] = src[i]; }
 
     dst[len] = '\0';
+}
+
+bool strcmp(const char *lhs, const char *rhs)
+{
+    if (lhs == nullptr || rhs == nullptr) { return false; }
+
+    while (*lhs != 0) {
+        if (*lhs != *rhs) { return false; }
+        ++lhs;
+        ++rhs;
+    }
+
+    return true;
+}
+
+String::String(const char *cstr)
+  : m_size{ strlen(cstr) },
+    m_data{ reinterpret_cast<char *>(malloc((m_size + 1) * sizeof(char))) }
+{
+    memcpy(m_data, cstr, m_size);
+    *(m_data + m_size) = '\0';
+}
+
+String::~String() { free(m_data); }
+
+String::String(const dts::String &other)
+  : m_size{ other.size() },
+    m_data{ reinterpret_cast<char *>(malloc((m_size + 1) * sizeof(char))) }
+{
+    memcpy(m_data, other.c_str(), other.size());
+    *(m_data + m_size) = '\0';
+}
+
+String::String(dts::String &&other) noexcept
+  : m_size{ move(other.size()) },
+    m_data{ move(other.data()) }
+{}
+
+String &String::operator=(const String &other)
+{
+    if (&other != this) {
+        m_size = other.m_size;
+        m_data = reinterpret_cast<char *>(malloc((m_size - 1) * sizeof(char)));
+        memcpy(m_data, other.c_str(), other.size());
+        *(m_data + m_size) = '\0';
+    }
+    return *this;
+}
+
+String &String::operator=(String &&other) noexcept
+{
+    m_size = move(other.size());
+    m_data = move(other.data());
+    return *this;
+}
+
+template<typename T>
+String String::from(const T number)
+{
+    char     buffer[32]  = { 0 };
+    dts::u32 buffer_size = 0;
+
+    for (dts::u32 i = number; i > 0; i /= 10) { buffer[buffer_size++] = "0123456789ABCDEF"[i % 10]; }
+
+    return reverse(String(buffer)); // NOLINT
+}
+
+String String::reverse(const dts::String &str)
+{
+    String   ret = str;
+    dts::u32 len = str.size();
+
+    for (dts::u32 i = 0; i < len / 2; ++i) { swap(ret[i], ret[len - i - 1]); }
+
+    return ret;
+}
+
+const char &String::operator[](const dts::u32 index) const { return m_data[index]; }
+
+char &String::operator[](const dts::u32 index) { return m_data[index]; }
+
+const char &String::at(const dts::u32 index) const
+{
+    assert(index < m_size - 1, "[ERROR] String::at(dts::u32 index): Index was greater than string size.");
+    return m_data[index];
+}
+
+char &String::at(const dts::u32 index)
+{
+    assert(index < m_size - 1, "[ERROR] String::at(dts::u32 index): Index was greater than string size.");
+    return m_data[index];
+}
+
+const char *String::c_str() const { return m_data; }
+
+char *String::data() const { return m_data; }
+
+const char &String::front() const { return m_data[0]; }
+
+char &String::front() { return m_data[0]; }
+
+const char &String::back() const { return m_data[m_size - 1]; }
+
+char &String::back() { return m_data[m_size - 1]; }
+
+const char *String::cbegin() const { return m_data; }
+
+char *String::begin() const { return m_data; }
+
+const char *String::cend() const { return m_data + m_size; }
+
+char *String::end() const { return m_data + m_size; }
+
+dts::u32 String::size() const { return m_size; }
+
+bool String::empty() const { return m_size > 0; }
+
+void String::clear() { memset(m_data, 0, m_size); }
+
+void String::push_back(const char ch)
+{
+    auto *new_data = reinterpret_cast<char *>(malloc((m_size + 2) * sizeof(char)));
+    memcpy(new_data, m_data, m_size - 1); // size - 1 not to copy the null termination character
+    free(m_data);
+
+    auto *new_char_spot = new_data + m_size + 1;
+    *new_char_spot      = ch;
+    *(++new_char_spot)  = '\0';
+
+    m_data = new_data;
+    ++m_size;
+}
+
+void String::pop_back() { --m_size; }
+
+String &String::operator+=(const char *other)
+{
+    const auto other_len = strlen(other);
+    auto      *new_data  = reinterpret_cast<char *>(malloc((m_size + other_len + 1) * sizeof(char)));
+    memcpy(new_data, m_data, m_size - 1);
+    free(m_data);
+
+    for (dts::u32 i = 0; i < other_len; ++i) {
+        new_data[i] = other[i];
+        ++m_size;
+    }
+
+    new_data[m_size + 1] = '\0';
+
+    m_data = new_data;
+    return *this;
+}
+
+String &String::operator+=(const dts::String &other)
+{
+    auto *new_data = reinterpret_cast<char *>(malloc(m_size + other.size() + 1 * sizeof(char)));
+    memcpy(new_data, m_data, m_size - 1);
+    free(m_data);
+
+    for (dts::u32 i = 0; i < other.size(); ++i) {
+        new_data[i] = other[i];
+        ++m_size;
+    }
+
+    new_data[m_size + 1] = '\0';
+
+    return *this;
+}
+
+bool String::operator==(const dts::String &rhs) const { return strcmp(c_str(), rhs.c_str()); }
+bool String::operator!=(const dts::String &rhs) const { return !strcmp(c_str(), rhs.c_str()); }
+
+bool String::starts_with(const char other) { return front() == other; }
+
+bool String::starts_with(const char *other)
+{
+    const auto other_len = strlen(other);
+
+    if (m_size < other_len) { return false; }
+
+    for (dts::u32 i = 0; i < other_len; ++i) {
+        if (m_data[i] != other[i]) { return false; }
+    }
+
+    return true;
+}
+
+bool String::starts_with(const dts::String &other)
+{
+    if (m_size < other.size()) { return false; }
+
+    for (dts::u32 i = 0; i < other.size(); ++i) {
+        if (m_data[i] != other[i]) { return false; }
+    }
+
+    return true;
+}
+
+bool String::ends_with(const char other) { return back() == other; }
+
+bool String::ends_with(const char *other)
+{
+    const auto other_len = strlen(other);
+
+    if (m_size < other_len) { return false; }
+
+    for (dts::u32 i = other_len - 1; i > 0; --i) {
+        if (m_data[i] != other[i]) { return false; }
+    }
+
+    return true;
+}
+
+bool String::ends_with(const dts::String &other)
+{
+    if (m_size < other.size()) { return false; }
+
+    for (dts::u32 i = other.size() - 1; i > 0; --i) {
+        if (m_data[i] == other[i]) { return false; }
+    }
+
+    return true;
+}
+
+bool String::contains(const char other)
+{
+    for (const auto it : *this) { // NOLINT
+        if (it == other) { return true; }
+    }
+
+    return false;
+}
+
+bool String::contains(const char *other)
+{
+    const auto other_len = strlen(other);
+
+    for (dts::u32 i = 0; i < other_len; ++i) {
+        if (m_data[i] == other[i]) {
+            const auto *lhs = m_data + i;
+            const auto *rhs = other + i;
+            while (*lhs == *rhs) {
+                ++lhs;
+                ++rhs;
+
+                if (*rhs != *lhs) { return false; }
+            }
+        }
+    }
+
+    return true;
+}
+
+bool String::contains(const String &other)
+{
+    for (dts::u32 i = 0; i < other.size(); ++i) {
+        if (m_data[i] == other[i]) {
+            const auto *lhs = m_data + i;
+            const auto *rhs = other.c_str() + i;
+            while (*lhs == *rhs) {
+                ++lhs;
+                ++rhs;
+
+                if (*rhs != *lhs) { return false; }
+            }
+        }
+    }
+
+    return true;
+}
+
+String String::substr(const dts::u32 pos, const dts::u32 len) const
+{
+    auto *new_str = reinterpret_cast<char *>(malloc(len + 1 * sizeof(char)));
+    memcpy(new_str, m_data + pos, len);
+    *(new_str + len + 1) = '\0';
+
+    return String(new_str);
+}
+
+dts::u32 String::find_first_of(const char other) const
+{
+    for (dts::u32 idx = 0; idx < size(); ++idx) {
+        if (m_data[idx] == other) { return idx; }
+    }
+
+    return npos;
+}
+
+dts::u32 String::find_first_of(const char *other) const
+{
+    const auto other_len = strlen(other);
+
+    dts::u32 idx = 0;
+    while (idx < other_len) {
+        if (m_data[idx] == other[idx]) {
+            const auto *lhs = m_data + idx;
+            const auto *rhs = other + idx;
+            while (*lhs == *rhs) {
+                ++lhs;
+                ++rhs;
+
+                if (*rhs != *lhs) { return npos; }
+            }
+        }
+    }
+
+    return idx;
+}
+
+dts::u32 String::find_first_of(const String &other) const
+{
+    dts::u32 idx = 0;
+    while (idx < other.size()) {
+        if (m_data[idx] == other[idx]) {
+            const auto *lhs = m_data + idx;
+            const auto *rhs = other.c_str() + idx;
+            while (*lhs == *rhs) {
+                ++lhs;
+                ++rhs;
+
+                if (*rhs != *lhs) { return npos; }
+            }
+        }
+    }
+
+    return idx;
+}
+
+dts::u32 String::find_last_of(const char other) const
+{
+    for (dts::u32 idx = size() - 1; idx > 0; --idx) {
+        if (m_data[idx] == other) { return idx; }
+    }
+
+    return npos;
+}
+
+dts::u32 String::find_last_of(const char *other) const
+{
+    const auto other_len = strlen(other);
+
+    dts::u32 idx = other_len;
+    while (idx > 0) {
+        if (m_data[idx] == other[idx]) {
+            const auto *lhs = m_data + idx;
+            const auto *rhs = other + idx;
+            while (*lhs == *rhs) {
+                ++lhs;
+                ++rhs;
+
+                if (*rhs != *lhs) { return npos; }
+            }
+        }
+    }
+
+    return idx;
+}
+
+dts::u32 String::find_last_of(const String &other) const
+{
+    dts::u32 idx = other.size();
+    while (idx > 0) {
+        if (m_data[idx] == other[idx]) {
+            const auto *lhs = m_data + idx;
+            const auto *rhs = other.c_str() + idx;
+            while (*lhs == *rhs) {
+                ++lhs;
+                ++rhs;
+
+                if (*rhs != *lhs) { return npos; }
+            }
+        }
+    }
+
+    return idx;
 }
 
 } // namespace dts
--- dts/String.hpp
+++ dts/String.hpp
@@ -1,12 +1,84 @@
 #pragma once
 
+#include <dts/Algorithm.hpp>
+#include <dts/Assert.hpp>
+#include <dts/Memory.hpp>
 #include <dts/Types.hpp>
+#include <dts/Utility.hpp>
 
 namespace dts {
 
-u32  strlen(char *str);
+u32  strlen(const char *str);
 void strcpy(char *dst, char *src);
+bool strcmp(const char *lhs, const char *rhs);
 
 char *itoa(u64 integer, u8 base = 10);
+
+class String
+{
+  public:
+    constexpr static dts::u32 npos = -1;
+
+    explicit String(const char *cstr);
+    ~String();
+    String(const String &other);
+    String(String &&other) noexcept;
+    String &operator=(const String &other);
+    String &operator=(String &&other) noexcept;
+
+    // TODO: Make this a Integral concept
+    // FIXME: Very basic implementation
+    template<typename T>
+    [[nodiscard]] static String from(const T number);
+
+    [[nodiscard]] static String reverse(const String &str);
+
+    [[nodiscard]] const char &operator[](const dts::u32 index) const;
+    [[nodiscard]] char       &operator[](const dts::u32 index);
+    [[nodiscard]] const char &at(const dts::u32 index) const;
+    [[nodiscard]] char       &at(const dts::u32 index);
+    [[nodiscard]] const char *c_str() const;
+    [[nodiscard]] char       *data() const;
+    [[nodiscard]] const char &front() const;
+    [[nodiscard]] char       &front();
+    [[nodiscard]] const char &back() const;
+    [[nodiscard]] char       &back();
+
+    [[nodiscard]] const char *cbegin() const;
+    [[nodiscard]] char       *begin() const;
+    [[nodiscard]] const char *cend() const;
+    [[nodiscard]] char       *end() const;
+
+    [[nodiscard]] dts::u32 size() const;
+    [[nodiscard]] bool     empty() const;
+
+    void                   clear();
+    void                   push_back(const char ch);
+    void                   pop_back();
+    [[nodiscard]] String  &operator+=(const char *other);
+    [[nodiscard]] String  &operator+=(const String &other);
+    [[nodiscard]] bool     operator==(const String &rhs) const;
+    [[nodiscard]] bool     operator!=(const String &rhs) const;
+    [[nodiscard]] bool     starts_with(const char other);
+    [[nodiscard]] bool     starts_with(const char *other);
+    [[nodiscard]] bool     starts_with(const String &other);
+    [[nodiscard]] bool     ends_with(const char other);
+    [[nodiscard]] bool     ends_with(const char *other);
+    [[nodiscard]] bool     ends_with(const String &other);
+    [[nodiscard]] bool     contains(const char other);
+    [[nodiscard]] bool     contains(const char *other);
+    [[nodiscard]] bool     contains(const String &other);
+    [[nodiscard]] String   substr(const dts::u32 pos, const dts::u32 len) const;
+    [[nodiscard]] dts::u32 find_first_of(const char other) const;
+    [[nodiscard]] dts::u32 find_first_of(const char *other) const;
+    [[nodiscard]] dts::u32 find_first_of(const String &other) const;
+    [[nodiscard]] dts::u32 find_last_of(const char other) const;
+    [[nodiscard]] dts::u32 find_last_of(const char *other) const;
+    [[nodiscard]] dts::u32 find_last_of(const String &other) const;
+
+  private:
+    dts::u32 m_size = 0;
+    char    *m_data = nullptr;
+};
 
 } // namespace dts
