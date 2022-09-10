ce11ce1084ee3e9deadade9dc8e368503fb28dee
dead-tech <dev.dead.tech@gmail.com>
2022-09-10 16:09:33 +0200

    :rocket: dts: Implement `count` algorithm

--- dts/Algorithm.hpp
+++ dts/Algorithm.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "Concepts.hpp"
+#include "Types.hpp"
+
 namespace dts {
 
 template<typename T>
@@ -8,6 +11,17 @@ constexpr void swap(T &a, T &b)
     const T &temp = a;
     a             = b;
     b             = temp;
+}
+
+template<IterableContainerC Container, typename Item>
+dts::u32 count(const Container &container, const Item &item)
+{
+    dts::u32 count = 0;
+    for (const auto &elem : container) {
+        if (elem == item) { ++count; }
+    }
+
+    return count;
 }
 
 } // namespace dts
