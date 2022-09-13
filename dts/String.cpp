30d1ab5921e4dad744806fdc5d69b966fa47d8c0
dead-tech <dev.dead.tech@gmail.com>
2022-09-13 12:10:44 +0200

    :sparkles: dts: Constexpr enable `strlen`

--- dts/String.cpp
+++ dts/String.cpp
@@ -2,14 +2,6 @@
 
 namespace dts {
 
-u32 strlen(const char *str)
-{
-    u32 len = 0;
-    while (str[len] != 0) { len++; }
-
-    return len;
-}
-
 void strcpy(char *dst, const char *src)
 {
     u32 len = 0;
--- dts/String.hpp
+++ dts/String.hpp
@@ -8,7 +8,14 @@
 
 namespace dts {
 
-u32  strlen(const char *str);
+constexpr u32 strlen(const char *str)
+{
+    u32 len = 0;
+    while (str[len] != 0) { len++; }
+
+    return len;
+}
+
 void strcpy(char *dst, char *src);
 bool strcmp(const char *lhs, const char *rhs);
 
