3010c5897ce69a4618775a94d119a31ba195f3b9
dead-tech <dev.dead.tech@gmail.com>
2022-09-13 14:45:39 +0200

    :rocket: dts: Implement `IndexSequence`

--- dts/Utility.hpp
+++ dts/Utility.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Types.hpp"
 #include "TypeTraits.hpp"
 
 namespace dts {
@@ -15,5 +16,48 @@ template<typename T>
 {
     return static_cast<T &&>(arg);
 }
+
+template<dts::u32... Integers>
+struct IndexSequence
+{
+    using type       = IndexSequence;
+    using value_type = dts::u32;
+    static constexpr dts::u32 size() noexcept { return sizeof...(Integers); }
+};
+
+namespace impl {
+
+template<typename FirstSequence, typename SecondSequence>
+struct MergeAndRenumber;
+
+template<dts::u32... FirstIntegers, dts::u32... SecondIntegers>
+struct MergeAndRenumber<
+  IndexSequence<FirstIntegers...>,
+  IndexSequence<SecondIntegers...>>
+  : IndexSequence<
+      FirstIntegers...,
+      (sizeof...(FirstIntegers) + SecondIntegers)...>
+{
+};
+
+} // namespace impl
+
+template<dts::u32 Size>
+struct MakeIndexSequence
+  : impl::MergeAndRenumber<
+      typename MakeIndexSequence<Size / 2>::type,
+      typename MakeIndexSequence<Size - Size / 2>::type>
+{
+};
+
+template<>
+struct MakeIndexSequence<0> : IndexSequence<>
+{
+};
+
+template<>
+struct MakeIndexSequence<1> : IndexSequence<0>
+{
+};
 
 } // namespace dts
